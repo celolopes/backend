f9f9ac9d40889f30e2adad7e86ff085870bd6887
Marcelo Lopes <marcelo.cae@gmail.com>
2022-07-04 19:18:18 -0300

    Arquivo loader

---
+++ loader.js
@@ -0,0 +1,16 @@
+// Express v4.16.0 e superior
+// --------------------------
+const port = 3003
+
+// const bodyParser = require('body-parser')
+const express = require('express');
+const server = express()
+
+server.use(express.json());
+server.use(express.urlencoded({
+  extended: true
+}));
+
+server.listen(port, function() {
+  console.log(`BACKEND is running on port ${port}.`)
+})
