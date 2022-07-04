d79f3c8a56521127d71caa5a851adeb599eeefb9
Marcelo Lopes <marcelo.cae@gmail.com>
2022-07-04 19:29:32 -0300

    Projeto Backend

--- loader.js
+++ loader.js
@@ -1,16 +1,2 @@
-// Express v4.16.0 e superior
-// --------------------------
-const port = 3003
-
-// const bodyParser = require('body-parser')
-const express = require('express');
-const server = express()
-
-server.use(express.json());
-server.use(express.urlencoded({
-  extended: true
-}));
-
-server.listen(port, function() {
-  console.log(`BACKEND is running on port ${port}.`)
-})
+require('./config/server')
+require('./config/database')
