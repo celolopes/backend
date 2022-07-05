c58e8bfdb047c496f830873ba54a40c3aa3367ff
Marcelo Lopes <marcelo.cae@gmail.com>
2022-07-05 20:20:59 -0300

    Novo Middleware criado de Sum

--- api/billingCycles/billingCyclesService.js
+++ api/billingCycles/billingCyclesService.js
@@ -5,4 +5,14 @@ const BillingCycles = require('./billingCycles')
 BillingCycles.methods(['get', 'post', 'put', 'delete'])
 BillingCycles.updateOptions({new: true, runValidators: true})
 
+BillingCycles.route('count', function(req, res, next) {
+  BillingCycles.count(function(error, value) {
+    if(error) {
+      res.status(500).json({erros: [error]})
+    } else {
+      res.json({value})
+    }
+  })
+})
+
 module.exports = BillingCycles
--- config/routes.js
+++ config/routes.js
@@ -11,4 +11,7 @@ module.exports = function(server) {
   // rotas da API
   const billingCyclesService = require('../api/billingCycles/billingCyclesService')
   billingCyclesService.register(router, '/billingCycles')
+
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
